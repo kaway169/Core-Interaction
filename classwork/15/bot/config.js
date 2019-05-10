dc1f578341164172d072a950826f760c6a34543a
Yuri Kawada <kaway169@newschool.edu>
2019-05-10 00:28:29 -0400

    update

---
+++ classwork/15/bot/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key: "j8GtTQ6a2i9VHwXRnI91HvH1A",
+	consumer_secret: "R5rpJVgqd1l7hjnXuyJLXpPookcpoO1hxmcPdtNnp4zkDUjeDP",
+	access_token: "1126273175388598279-HTaEBdr2G4k8cyFPvVbn5B1lu3HyHO",
+	access_token_secret: "sgimY9GzdKdZpJMOPtNfFECD4t2Q3nz6AolFGtDbk7FSH"
+}
---
+++ classwork/15/bot/index.js
@@ -0,0 +1,49 @@
+const twitter_handle = "foodgurts";
+
+//twit is a package
+var Twit = require("twit");
+var config = require("./config");
+
+var T = new Twit(config);
+
+console.log("the bot is starting...");
+
+function sendTweet(message){
+	var tweet = {status: message};
+	T.post("statuses/update", tweet, tweeted);
+
+	function tweeted(error, data, response){
+		if (error){
+			console.log(error);
+		}
+		// console.log(data);
+		console.log("tweeted", message);
+	}
+
+}
+
+// sending one tweet when the app boots up
+// sendTweet("🙈🙉🙊");
+
+// once every 24 hrs
+// setInterval(function(){}, 1000 * 60 * 60 * 24)
+
+function randInt(max){
+	return Math.floor( Math.random() * max );
+}
+
+function gardenTweet(){
+	var chars = ["🐒","😩","💦","💦","🤪","🐒","🦄","🐽","🐷","🦀","🌈","⚡️","✨","🌚"];
+	var garden = "";
+
+	for (var i = 0; i < 16; i+= 1){
+		garden = garden + chars[randInt(chars.length)];
+	}
+	sendTweet(garden);
+}
+
+setInterval(gardenTweet, 1000 * 60 * 10)
+
+gardenTweet();
+
+
